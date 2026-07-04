0c0276a2896a0cb8890f29a183557e37bc5c7677
Rajshree Ingle <ingle.rajshree@gmail.com>
2026-07-04 21:52:46 +0530

    Time: 1 ms (97.64%) | Memory: 42.5 MB (61.01%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==1 || x==0){
+            return x;
+        }
+
+        long start = 1;
+        long end = x;
+        long mid =0;
+
+        while(start<=end){
+            mid = start + (end-start)/2;
+            if(mid*mid > (long)x){
+                end = mid-1;
+            }else if(mid*mid==x){
+                return (int)mid;
+            }else{
+                start=mid+1;
+            }
+        }
+        return Math.round(end);
+    }
+}
